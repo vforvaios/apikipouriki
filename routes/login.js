b7c62ea80343231ae35b6dd1fbed88df1965c202
Vaios Anastasiadis <vaios_a@outlook.com>
2025-02-17 13:15:11 +0200

    Add login real functionality

--- routes/login.js
+++ routes/login.js
@@ -4,5 +4,6 @@ const Login = require('../controllers/login.controller');
 const router = express.Router();
 
 router.post('/', Login.loginUser);
+router.post('/register', Login.registerUser);
 
 module.exports = router;
