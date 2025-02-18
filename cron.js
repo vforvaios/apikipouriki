8c325802d442c1c7bbc696fcbcc032163945f4ae
Vaios Anastasiadis <vaios_a@outlook.com>
2025-02-18 16:18:45 +0200

    Finalize cron job'

--- cron.js
+++ cron.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const cron = require('node-cron');
 const db = require('./services/db');
+
 const scheduleAdditionOfNewDates = async () => {
   try {
     const now = new Date();
@@ -14,17 +15,29 @@ const scheduleAdditionOfNewDates = async () => {
       [1],
     );
 
-    console.log(
-      new Date(secondFridayOfTheTwoWeeks?.[0]?.startDate2).toLocaleDateString(),
+    const lastMondayInDb = new Date(secondFridayOfTheTwoWeeks?.[0]?.startDate2);
+    const temp = new Date(secondFridayOfTheTwoWeeks?.[0]?.startDate2);
+    temp.setDate(temp.getDate() + 5);
+
+    const firstUpcomingMonday = new Date(
+      lastMondayInDb.setDate(lastMondayInDb.getDate() + 7),
     );
-    console.log(now.toLocaleDateString());
+    const secondUpcomingMonday = new Date(
+      lastMondayInDb.setDate(lastMondayInDb.getDate() + 7),
+    );
+    if (new Date(temp).getTime() <= now.getTime()) {
+      await db.query(
+        `INSERT INTO dates(startDate1, startDate2, isActive) VALUES(?,?,?)`,
+        [firstUpcomingMonday, secondUpcomingMonday, 1],
+      );
+    }
   } catch (error) {
     console.log(error);
   }
 };
 // Running the job at 00:01 AM Greece time
 cron.schedule(
-  //   '1 0 * * *',
+  // '1 0 * * *',
   '* * * * *',
   () => {
     scheduleAdditionOfNewDates();
