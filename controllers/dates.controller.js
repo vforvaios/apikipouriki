3850c7be0155258e6a1016c1ff1d953adbdb35df
Vaios Anastasiadis <vaios_a@outlook.com>
2025-02-19 17:11:41 +0200

    Fix for cron

--- controllers/dates.controller.js
+++ controllers/dates.controller.js
@@ -22,4 +22,42 @@ const getLastDates = async (req, res, next) => {
   }
 };
 
-module.exports = { getLastDates };
+const scheduleAdditionOfNewDates = async (req, res, next) => {
+  try {
+    const now = new Date();
+    const [secondFridayOfTheTwoWeeks] = await db.query(
+      `
+        SELECT startDate2 
+         FROM dates WHERE isActive=?
+         ORDER BY id desc 
+         LIMIT 1
+        `,
+      [1],
+    );
+
+    const lastMondayInDb = new Date(secondFridayOfTheTwoWeeks?.[0]?.startDate2);
+    const temp = new Date(secondFridayOfTheTwoWeeks?.[0]?.startDate2);
+    temp.setDate(temp.getDate() + 5);
+
+    const firstUpcomingMonday = new Date(
+      lastMondayInDb.setDate(lastMondayInDb.getDate() + 7),
+    );
+    const secondUpcomingMonday = new Date(
+      lastMondayInDb.setDate(lastMondayInDb.getDate() + 7),
+    );
+    if (new Date(temp).getTime() <= now.getTime()) {
+      await db.query(
+        `INSERT INTO dates(startDate1, startDate2, isActive) VALUES(?,?,?)`,
+        [firstUpcomingMonday, secondUpcomingMonday, 1],
+      );
+    }
+    res.sendStatus(200);
+  } catch (error) {
+    res.status(401).json({
+      error,
+    });
+    next(error);
+  }
+};
+
+module.exports = { getLastDates, scheduleAdditionOfNewDates };
--- routes/dates.js
+++ routes/dates.js
@@ -4,5 +4,6 @@ const Dates = require('../controllers/dates.controller');
 const router = express.Router();
 
 router.get('/', Dates.getLastDates);
+router.get('/schedule', Dates.scheduleAdditionOfNewDates);
 
 module.exports = router;
