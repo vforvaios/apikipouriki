1553524a3d7bb2d624d59009c1af21f816a2bf7f
Vaios Anastasiadis <vaios_a@outlook.com>
2025-02-24 11:00:10 +0200

    Addition in current schedule api

--- controllers/schedules.controller.js
+++ controllers/schedules.controller.js
@@ -15,7 +15,7 @@ const getCurrentSchedule = async (req, res, next) => {
 
     const [currentSchedule] = await db.query(
       `
-        SELECT s.id, s.datesId, 
+        SELECT distinct s.id, s.datesId, 
                sd.carId as car, sd.numberOfDay, sd.draggableItemIds as drivers, 
                sr.draggableItemIds as regions
         FROM schedules s
@@ -26,7 +26,49 @@ const getCurrentSchedule = async (req, res, next) => {
       [dateId],
     );
 
-    res.status(200).json({ currentSchedule });
+    let drivers = [];
+    let regions = [];
+    const getAllRelevantDrivers = currentSchedule.map(async (sched, index) => {
+      const [itmDrivers] = await db.query(
+        `
+          SELECT * FROM draggable_items WHERE id IN (${sched.drivers
+            .split(',')
+            ?.map((reg) => reg.toString())
+            .join()})`,
+      );
+
+      drivers.push(...itmDrivers);
+    });
+
+    const getAllRelevantRegions = currentSchedule.map(async (sched, index) => {
+      const [itmRegions] = await db.query(
+        `
+          SELECT * FROM draggable_items WHERE id IN (${sched.regions
+            .split(',')
+            ?.map((reg) => reg.toString())
+            .join()})`,
+      );
+      regions.push(...itmRegions);
+    });
+
+    await Promise.all(getAllRelevantDrivers);
+    await Promise.all(getAllRelevantRegions);
+
+    const newCurrentSchedule = currentSchedule.map((currSched) => {
+      const currDrivers = currSched.drivers.split(',').map((itm) => Number(itm));
+      const currRegions = currSched.regions.split(',').map((itm) => Number(itm));
+      return {
+        ...currSched,
+        drivers: Array.from(new Set(drivers.map((obj) => JSON.stringify(obj))))
+          .map((e) => JSON.parse(e))
+          .filter((dr) => currDrivers.includes(dr.id)),
+        regions: Array.from(new Set(regions.map((obj) => JSON.stringify(obj))))
+          .map((e) => JSON.parse(e))
+          .filter((dr) => currRegions.includes(dr.id)),
+      };
+    });
+
+    res.status(200).json({ currentSchedule: newCurrentSchedule });
   } catch (error) {}
 };
 
